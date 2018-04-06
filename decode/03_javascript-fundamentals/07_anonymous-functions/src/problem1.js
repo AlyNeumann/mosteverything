cd938cae3d50af625fb3fca8938a8aa1faf13ef0
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-04-06 17:46:31 -0400

    anom

--- decode/03_javascript-fundamentals/07_anonymous-functions/src/problem1.js
+++ decode/03_javascript-fundamentals/07_anonymous-functions/src/problem1.js
@@ -1,12 +1,24 @@
 // fix all the errors
+
 function c(g, h) {
     var x = g(6);
     var y = h(8);
-    return [x, y];
+     return [x, y];
 }
 
 function t() {
-    return c(function (x) {return y + 1}, function (y) {return x * 2});
+
+    return c(function (x) {return x + 1;}, function (y) {return y * 2;})
 }
+// return c((function (x) {return y + 1;}), (function (y) {return y * 2;}));
+// var y = 8;
+// var x = 6;
+// (function(){
+//     return (function(x){return y + 1}), (function (y){return x * 2});
+// }
+//  (function(g,h){
+//     return [x, y];
+//  }))
+
 
 module.exports = t;
