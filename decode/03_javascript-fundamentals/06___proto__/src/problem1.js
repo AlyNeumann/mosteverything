5d4ec5b01e855c3748486b2d213e8b86729d0bc5
Allison Neumann <allisonneumann@Allisons-MacBook-Pro.local>
2018-04-06 15:58:49 -0400

    test

--- decode/03_javascript-fundamentals/06___proto__/src/problem1.js
+++ decode/03_javascript-fundamentals/06___proto__/src/problem1.js
@@ -1,6 +1,9 @@
 // Make parent the __proto__ of child
 
+// console.log("Hey Aly");
+
 var parent = {x: 5, y: 6, z: 8};
 var child = {x : 10};
+child.__proto__ = parent; 
 
 module.exports = {parent, child}
